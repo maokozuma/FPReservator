3de85ab6b5770d8a53fc6be99d8c0c3652d823ad
MaoKozuma <kozuma.mao@moneyforward.co.jp>
2018-06-04 15:57:21 +0900

    Add uniqueness of email

--- app/models/fp.rb
+++ app/models/fp.rb
@@ -3,6 +3,6 @@ class Fp < ApplicationRecord
   validates :name, length: { maximum: 255 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
-
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
